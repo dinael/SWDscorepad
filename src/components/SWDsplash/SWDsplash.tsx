e34737dae5c26a745335cdf0477de65877abe952
Dinael URDANETA <dinael@gmail.com>
2023-09-24 11:54:11 +0200

    comment code to work

--- src/components/SWDsplash/SWDsplash.tsx
+++ src/components/SWDsplash/SWDsplash.tsx
@@ -1,36 +1,36 @@
-import { useGameContext } from '../../providers/GameContext';
+//import { useGameContext } from '../../providers/GameContext';
 
-function Configuration() {
-  const {
-    player1,
-    setPlayer1,
-    player2,
-    setPlayer2,
-    handleConfigChange,
-    startGame,
-  } = useGameContext();
+// function Configuration() {
+//   const {
+//     player1,
+//     setPlayer1,
+//     player2,
+//     setPlayer2,
+//     handleConfigChange,
+//     startGame,
+//   } = useGameContext();
 
-  return (
-    <div>
-      <h2>Configuración del juego</h2>
-      <input
-        type="text"
-        placeholder="Nombre del jugador 1"
-        value={player1}
-        onChange={(e) => setPlayer1(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Nombre del jugador 2"
-        value={player2}
-        onChange={(e) => setPlayer2(e.target.value)}
-      />
-      <button onClick={() => handleConfigChange('agora')}>Agora</button>
-      <button onClick={() => handleConfigChange('pantheon')}>Pantheon</button>
-      <button onClick={() => handleConfigChange('solo')}>Solo</button>
-      <button onClick={startGame}>Comenzar</button>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h2>Configuración del juego</h2>
+//       <input
+//         type="text"
+//         placeholder="Nombre del jugador 1"
+//         value={player1}
+//         onChange={(e) => setPlayer1(e.target.value)}
+//       />
+//       <input
+//         type="text"
+//         placeholder="Nombre del jugador 2"
+//         value={player2}
+//         onChange={(e) => setPlayer2(e.target.value)}
+//       />
+//       <button onClick={() => handleConfigChange('agora')}>Agora</button>
+//       <button onClick={() => handleConfigChange('pantheon')}>Pantheon</button>
+//       <button onClick={() => handleConfigChange('solo')}>Solo</button>
+//       <button onClick={startGame}>Comenzar</button>
+//     </div>
+//   );
+// }
 
-export default Configuration;
+// export default Configuration;
